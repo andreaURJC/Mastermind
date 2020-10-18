a5a582048de49bf89a2ae2e021de6c614f4356d0
andrea <andreacolina@MacBook-Pro-de-Andrea.local>
2020-10-18 11:15:25 +0200

    dv. withFactoryMethod

--- src/com/mastermind/MastermindConsole.java
+++ src/com/mastermind/MastermindConsole.java
@@ -3,18 +3,10 @@ package com.mastermind;
 import com.mastermind.models.Game;
 import com.mastermind.views.console.View;
 
-class MastermindConsole {
-    private Game game;
-
-    private View view;
-
-    private MastermindConsole() {
-        this.game = new Game();
-        this.view = new View(game);
-    }
-
-    private void play() {
-        this.view.interact();
+class MastermindConsole extends Mastermind {
+    @Override
+    protected com.mastermind.views.View createView(Game game) {
+        return new View(game);
     }
 
     public static void main(String[] args) {
--- src/com/mastermind/MastermindGraphics.java
+++ src/com/mastermind/MastermindGraphics.java
@@ -3,23 +3,13 @@ package com.mastermind;
 import com.mastermind.models.Game;
 import com.mastermind.views.graphics.View;
 
-public class MastermindGraphics {
-
-    private Game game;
-
-    private View view;
-
-    private MastermindGraphics() {
-        this.game = new Game();
-        this.view = new View(game);
-    }
-
-    private void play() {
-        this.view.interact();
+public class MastermindGraphics extends Mastermind {
+    @Override
+    protected com.mastermind.views.View createView(Game game) {
+        return new View(game);
     }
 
     public static void main(String[] args) {
         new MastermindGraphics().play();
     }
-
 }
